b58e5fff7c0f8580b965ca2514de4ccc185ec562
xavi_navas <sergio.navas.7e8@itb.cat>
2024-12-16 00:57:49 +0100

    maquinabill etes sin acabar

--- src/main/kotlin/controllers/maquinaBillets.kt
+++ src/main/kotlin/controllers/maquinaBillets.kt
@@ -23,17 +23,18 @@ fun menuBilletes(){
         val opcio= scanner.nextInt()
         when(opcio){
             1-> sencill()
-            2-> menuZona()
-            3-> menuZona()
-            4-> menuZona()
-            5->  menuZona()
+            2-> tCasual()
+            3-> mostrarMenu()
+            4-> mostrarMenu()
+            5-> mostrarMenu()
             6-> {
                 println("Adeu, que tingui un bon dia.")
                 continuar=false
             }
             else-> {
-                println("Opcio incorreecta")
-                continuar = false
+                println("Opcio no disponible, torna a escollir una:")
+
+
             }
         }
     }
@@ -51,17 +52,72 @@ fun mostrarMenuZona() {
 fun sencill(){
     val scanner= Scanner(System.`in`)
     mostrarMenuZona()
-    val billSencill= 2.40
-    val continuar= true
+
+    var continuar= true
     while(continuar){
         val opcio= scanner.nextInt()
+        val (preuZona1, preuZona2, preuZona3) = calcularPreu(2.40)
         when(opcio){
-            1-> print("De cuantes zones?:")
-            2-> print("De cantes zones?:")
-            3-> print("De cuantes zones?:")
+            1-> println("Billet sencill, Zona1\nEl preu del billet es $preuZona1€")
+            2-> println("Billet sencill, Zona2\nEl preu del billet es $preuZona2€")
+            3-> println("Billet sencill, Zona3\nEl preu del billet es $preuZona3€")
             4-> return mostrarMenu()
 
         }
+        println("Vols continuar comprant? (Sí/No):")
+        val resposta = scanner.next()
+        if (resposta=="SI"|| resposta == "si" || resposta == "Si"){
+            return mostrarMenu()
+
+        }else if (resposta == "No" || resposta == "no" || resposta == "NO") {
+            continuar = false
+            println("Gràcies per la teva compra! Fins aviat.")
+        }
+    }
+}
+fun tCasual(){
+    val scanner= Scanner(System.`in`)
+    mostrarMenuZona()
+
+    var continuar= true
+
+    while(continuar){
+        val opcio= scanner.nextInt()
+        val (preuZona1, preuZona2, preuZona3) = calcularPreu(11.35)
+        when(opcio){
+            1-> println("Billet Tcasual, Zona1\nEl preu del billet es $preuZona1€")
+            2-> println("Billet Tcasual, Zona2\nEl preu del billet es $preuZona2€")
+            3-> println("Billet Tcasual, Zona3\nEl preu del billet es $preuZona3€")
+            4-> return mostrarMenu()
+
+        }
+    }
+    mesBillets()
+}
+fun calcularPreu(preuZona1: Double): Triple<Float, Float, Float> {
+    val preu = preuZona1.toFloat()
+    val preuZona2 = (preuZona1 * 1.3125).toFloat()
+    val preuZona3 = (preuZona1 * 1.8443).toFloat()
+    return Triple(preu, preuZona2, preuZona3)
+}
+fun mesBillets(){
+    val scanner= Scanner(System.`in`)
+    println("Vols continuar comprant? (Sí/No):")
+    var seguirCompran= 0
+    var continuar= true
+    val resposta = scanner.next()
+    if (resposta=="SI"|| resposta == "si" || resposta == "Si"){
+        seguirCompran++
+        if (seguirCompran>=3){
+            println("Error: Esperi a l'operari")
+            continuar = false
+        }else {
+            mostrarMenu()
+        }
+
+    }else if (resposta == "No" || resposta == "no" || resposta == "NO") {
+        continuar = false
+        println("Gràcies per la teva compra! Fins aviat.")
     }
 
 }
